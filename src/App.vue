5957c8a91fd191f1f8a0f8b1c4899271c1bbc9c8
Youssef samih <youssef.samih97@gmail.com>
2024-12-08 00:58:51 +0000

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,63 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <header class="top-bar spread">
+    <nav class="top-bar-nav">
+      <router-link to="/" class="top-bar-link">
+        <i class="icofont-spoon-and-fork"></i>
+        <span>Home</span>
+      </router-link>
+      <router-link to="/products" class="top-bar-link">
+        <span>Products</span>
+      </router-link>
+      <router-link to="/past-orders" class="top-bar-link">
+        <span>Past Orders</span>
+      </router-link>
+    </nav>
+    <a @click="toggleSidebar" class="top-bar-cart-link">
+      <i class="icofont-cart-alt icofont-1x"></i>
+      <span>Cart ({{ totalQuantity }})</span>
+    </a>
+  </header>
+
+  <router-view :inventory="inventory" :addToCart="addToCart" />
+
+  <SidebarCart v-if="showSidebar" :cart="cart" :toggle="toggleSidebar" :inventory="inventory"
+    :remove="removeFromCart" />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import SidebarCart from '@/components/SidebarCart.vue'
+import food from './food.json'
 
-nav {
-  padding: 30px;
-}
+export default {
+  components: {
+    SidebarCart
+  },
+  data () {
+    return {
+      showSidebar: false,
+      inventory: food,
+      cart: {}
+    }
+  },
+  computed: {
+    totalQuantity () {
+      return Object.values(this.cart).reduce((total, quantity) => total + quantity, 0)
+    }
+  },
+  methods: {
+    addToCart (name, quantity) {
+      if (!this.cart[name]) {
+        this.cart[name] = 0
+      }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+      this.cart[name] += quantity
+    },
+    toggleSidebar () {
+      this.showSidebar = !this.showSidebar
+    },
+    removeFromCart (name) {
+      delete this.cart[name]
+    }
+  }
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="splash-container">
+      <div class="splash">
+        <h1>Splendid Food</h1>
+      </div>
+    </div>
+
+    <main class="wrapper">
+
+      <h2>Recommended</h2>
+
+      <div class="recommended">
+
+        <ProductCart
+          v-for="product in inventory.slice(0, 3)"
+          :key="product.id"
+          :product="product"
+          :addToCart="addToCart"
+        />
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import ProductCart from '@/components/ProductCart.vue'
 
 export default {
   name: 'HomeView',
+  props: ['inventory', 'addToCart'],
   components: {
-    HelloWorld
+    ProductCart
   }
 }
 </script>
